38d255d356116cc869b06e9b4cbe8f8988a888e6
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2023-03-17 13:46:31 +0300

    Configuration pour JSF

---
+++ src/main/java/com/gnllsn/tpcustomer52rasoanaivoginoallison/config/ConfigJSF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gnllsn.tpcustomer52rasoanaivoginoallison.config;
+
+/**
+ *
+ * @author Grasoanaivo
+ * 
+ * classe de configuration  permettra d'ajouter d'autres configurations, par exemple pour la sécurité.
+ */
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
