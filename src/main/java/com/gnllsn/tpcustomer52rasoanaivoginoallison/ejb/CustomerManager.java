8cfc60cd92a530adcb3b9018902572fe441b2a86
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2023-03-22 19:47:47 +0300

    Ajout d'un backing bean pour la page des détails

--- src/main/java/com/gnllsn/tpcustomer52rasoanaivoginoallison/ejb/CustomerManager.java
+++ src/main/java/com/gnllsn/tpcustomer52rasoanaivoginoallison/ejb/CustomerManager.java
@@ -13,30 +13,32 @@ import jakarta.persistence.Query;
 
 /**
  *
- * @author Grasoanaivo
- * on crée une "façade" pour les opérations élémentaires sur les clients : création, suppression, recherche, modification.
-*/
-
-
+ * @author Grasoanaivo on crée une "façade" pour les opérations élémentaires sur
+ * les clients : création, suppression, recherche, modification.
+ */
 @Stateless
 public class CustomerManager {
-    
+
     @PersistenceContext(unitName = "customerPU")
     private EntityManager em;
-    
+
     /*va exécuter une requête JPQL dont le nom est "Customer.findAll", qui est définie dans l'entité Customer déjà écrite (la requête correspond à un "select *" sur les clients).*/
-    public List<Customer> getAllCustomers() {  
+    public List<Customer> getAllCustomers() {
         Query query = em.createNamedQuery("Customer.findAll");
-       return query.getResultList();
-    }  
+        return query.getResultList();
+    }
+
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
+    }
 
     /*donne à gérer à l'entity manager le customer passé en paramètre (méthode merge dont les finesses seront étudiées dans le cours JPA). Au moment du commit qui aura lieu à la fin de la méthode comme vous le verrez dans le cours sur les EJB, les modifications apportées à customer depuis sa création seront enregistrées dans la base de données. Le plus souvent update génèrera un UPDATE SQL dans la base de données.*/
     public Customer update(Customer customer) {
-      return em.merge(customer);  
-    }  
+        return em.merge(customer);
+    }
 
     /*ressemble à update mais le paramètre de type Customer ne doit pas correspondre à des données déjà dans la base de données. persist génèrera un INSERT SQL dans la base de données.*/
     public void persist(Customer customer) {
-       em.persist(customer);
-    }    
+        em.persist(customer);
+    }
 }
